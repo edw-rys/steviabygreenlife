749be1eba4265662cc26591593ca19f42d3d69a3
edw-rys <edw-toni@hotmail.com>
2026-02-04 13:03:34 -0500

    Update php version

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,113 +2,51 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * This namespace is applied to your controller routes.
+     * The path to your application's "home" route.
      *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'App\Http\Controllers';
-
-    /**
-     * The path to the "home" route for your application.
+     * Typically, users are redirected here after authentication.
      *
      * @var string
      */
     public const HOME = '/home';
 
     /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
+     * Define your route model bindings, pattern filters, and other route configuration.
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
 
-        parent::boot();
-    }
+        $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->namespace('App\Http\Controllers\Api')
+                ->group(base_path('routes/api.php'));
 
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map()
-    {
-        $this->mapApiRoutes();
+            Route::middleware('web')
+                ->namespace('App\Http\Controllers')
+                ->group(base_path('routes/web.php'));
 
-        $this->mapWebRoutes();
+            Route::middleware('web')
+                ->namespace('App\Http\Controllers\Front')
+                ->group(base_path('routes/front.php'));
 
-        $this->mapFrontRoutes();
-
-        $this->mapAdminRoutes();
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/web.php'));
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapFrontRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->namespace . '\Front')
-            ->group(base_path('routes/front.php'));
-    }
-
-    
-    /**
-     * Define the "admin" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapAdminRoutes()
-    {
-        Route::middleware(['web', 'auth', 'onlyAdmin'])
-            ->namespace($this->namespace . '\Admin')
-            ->name('admin.')
-            ->prefix('admin')
-            ->group(base_path('routes/admin.php'));
-    }
-
-
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-            ->middleware('api')
-            ->namespace($this->namespace .'\Api')
-            ->group(base_path('routes/api.php'));
+            Route::middleware(['web', 'auth', 'onlyAdmin'])
+                ->namespace('App\Http\Controllers\Admin')
+                ->name('admin.')
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
+        });
     }
 }
